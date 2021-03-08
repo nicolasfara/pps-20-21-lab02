7d4feee4cf4f5c1d266483308319a6bc61caae2a
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-08 11:57:21 +0100

    Finish functions

--- src/u02/es/Functions.scala
+++ src/u02/es/Functions.scala
@@ -2,4 +2,17 @@ package u02.es
 
 object Functions {
 
+  val parityLiteral: Int => String = {
+    case v if v % 2 == 0 => "Even"
+    case _ => "Odd"
+  }
+
+  def parityMethod(value: Int): String = value match {
+    case v if v % 2 == 0 => "Even"
+    case _ => "Odd"
+  }
+
+  val neg: (String => Boolean) => String => Boolean = (pred: String => Boolean) => (x: String) => !pred(x)
+
+  def negMethod[A](predicate: A => Boolean): A => Boolean = x => !predicate(x)
 }
