746da85f65bc0eeb057c67dfb8bebc81323d6bd3
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-08 12:05:37 +0100

    Fix fibonacci code

--- src/u02/es/Fibonacci.scala
+++ src/u02/es/Fibonacci.scala
@@ -5,16 +5,17 @@ import scala.annotation.tailrec
 object Fibonacci {
 
   def fib(value: Int): Int = value match {
-    case v if v > 0 => value + fib(value-1)
+    case v if v > 1 => fib(value-1) + fib(value - 2)
+    case v if v == 1 => 1
     case _ => 0
   }
 
   def fibTailRec(value: Int): Int = {
     @tailrec
-    def _fib(value: Int, current: Int): Int = value match {
-      case v if v > 0 => _fib(value - 1, current + value)
-      case _ => current
+    def _fib(value: Int, first: Int, second: Int): Int = value match {
+      case v if v > 0 => _fib(value - 1, second, first + second)
+      case _ => first
     }
-    _fib(value, 0)
+    _fib(value, 0, 1)
   }
 }
--- test/u02/es/FibonacciTest.scala
+++ test/u02/es/FibonacciTest.scala
@@ -7,15 +7,19 @@ import u02.es.Fibonacci._
 class FibonacciTest {
 
   @Test def fibonacciTest(): Unit = {
+    assertEquals(0, fib(0))
     assertEquals(1, fib(1))
-    assertEquals(3, fib(2))
-    assertEquals(6, fib(3))
+    assertEquals(1, fib(2))
+    assertEquals(2, fib(3))
+    assertEquals(3, fib(4))
   }
 
   @Test def fibonacciTailRecTest(): Unit = {
+    assertEquals(0, fibTailRec(0))
     assertEquals(1, fibTailRec(1))
-    assertEquals(3, fibTailRec(2))
-    assertEquals(6, fibTailRec(3))
+    assertEquals(1, fibTailRec(2))
+    assertEquals(2, fibTailRec(3))
+    assertEquals(3, fibTailRec(4))
   }
 
 }
