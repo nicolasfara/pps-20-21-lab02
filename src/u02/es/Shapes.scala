a44179104a946570a96abb4a242180140277644e
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-11 21:24:09 +0100

    Rename filed for shapes class

--- src/u02/es/Shapes.scala
+++ src/u02/es/Shapes.scala
@@ -2,19 +2,19 @@ package u02.es
 
 object Shapes {
   sealed trait Shape
-  case class Rectangle(l1: Double, l2: Double) extends Shape
+  case class Rectangle(width: Double, height: Double) extends Shape
   case class Circle(radius: Double) extends Shape
-  case class Square(l: Double) extends Shape
+  case class Square(side: Double) extends Shape
 
   def perimeter(shape: Shape): Double = shape match {
-    case Rectangle(l1, l2) => l1 * 2 + l2 * 2
+    case Rectangle(width, height) => width * 2 + height * 2
     case Circle(radius) => radius * 2 * Math.PI
-    case Square(l) => l * 4
+    case Square(side) => side * 4
   }
 
   def area(shape: Shape): Double = shape match {
-    case Rectangle(l1, l2) => l1 * l2
+    case Rectangle(width, height) => width * height
     case Circle(radius) => radius * radius * Math.PI
-    case Square(l) => l * l
+    case Square(side) => side * side
   }
 }
