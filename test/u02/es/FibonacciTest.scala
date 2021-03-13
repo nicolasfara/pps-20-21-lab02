75c572da180b05b5f978d852402805dfdb2df843
nic0lasfara <nicolas.farabegoli@gmail.com>
2021-03-13 11:33:28 +0100

    Improve test for fibonacci for testing base and tail rec with big numbers

--- test/u02/es/FibonacciTest.scala
+++ test/u02/es/FibonacciTest.scala
@@ -1,8 +1,13 @@
 package u02.es
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.{assertEquals, assertTimeout}
+import org.junit.jupiter.api.{Assertions, Test}
+import org.junit.jupiter.api.function.Executable
+import org.opentest4j.AssertionFailedError
 import u02.es.Fibonacci._
+
+import java.time.Duration.ofSeconds
+
 
 class FibonacciTest {
 
@@ -14,6 +19,24 @@ class FibonacciTest {
 
   @Test def fibonacciTailRecTest(): Unit = {
     testFibonacci(fibTailRec)
+  }
+
+  @Test def testBigNumber(): Unit = {
+    Assertions.assertThrows(classOf[AssertionFailedError], () => {
+      assertTimeout(ofSeconds(2), new Executable {
+        override def execute(): Unit = {
+          fib(48)
+        }
+      })
+    })
+  }
+
+  @Test def testBigNumberTailRec(): Unit = {
+    assertTimeout(ofSeconds(2), new Executable {
+      override def execute(): Unit = {
+        assertEquals(512559680, fibTailRec(48))
+      }
+    })
   }
 
   private def testFibonacci(fibonacci: Int => Int): Unit = {
